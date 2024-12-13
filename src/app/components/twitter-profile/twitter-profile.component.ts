da4642f78c8cba1a03c93f0e311c20b5f09be2cf
Huy <106498928+Vanhuyne@users.noreply.github.com>
2024-12-13 22:06:21 +0700

    feat: enhance Twitter profile component with additional user data and styling; update styles and layout for better presentation

--- src/app/components/twitter-profile/twitter-profile.component.ts
+++ src/app/components/twitter-profile/twitter-profile.component.ts
@@ -1,36 +1,47 @@
 import { Component } from '@angular/core';
 import { UserProfile } from '../../interface/UserProfile';
+import { DomSanitizer, SafeResourceUrl } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-twitter-profile',
   templateUrl: './twitter-profile.component.html',
-  styleUrl: './twitter-profile.component.css'
+  styleUrls: ['./twitter-profile.component.css']
 })
 export class TwitterProfileComponent {
-  profile: UserProfile = {
-    name: 'Van Huy',
-    handle: '@ThanVanHuy25640',
-    bio: 'Software Developer | Powered by caffeine ☕ , exercise every day 🚴‍♀️',
-    followers: 50,
-    following: 42,
-    joined: 'July 2024',
-    profileImage: 'assets/avatar.png',
-    coverImage: 'https://pbs.twimg.com/profile_banners/1866019820153507840/1733888976/1500x500',
-    tweets: [
-      {
-        id: 1,
-        text: 'Excited to help developers build amazing web applications!',
-        likes: 5432,
-        retweets: 987,
-        timestamp: '2h'
-      },
-      {
-        id: 2,
-        text: 'Angular and Tailwind make building modern web interfaces a breeze.',
-        likes: 3210,
-        retweets: 654,
-        timestamp: '1d'
-      }
-    ]
-  };
+  profile: UserProfile;
+
+  activeTab: string = 'Post';
+
+  constructor(private sanitizer: DomSanitizer) {
+    this.profile = {
+      name: 'Van Huy',
+      handle: '@ThanVanHuy25640',
+      bio: 'Software Developer | Powered by caffeine ☕ , exercise every day 🚴‍♀️',
+      followers: 50,
+      following: 42,
+      website: 'https://github.com/Vanhuyne',
+      joined: 'July 2024',
+      location: 'Viet Nam',
+      profileImage: 'assets/avatar.png',
+      coverImage: 'https://pbs.twimg.com/profile_banners/1866019820153507840/1733888976/1500x500',
+      tweets: [
+        {
+          id: 1,
+          text: 'People look at me like I\'m a weirdo! eccentric! 🤪 I just want to be myself!',
+          likes: 4,
+          retweets: 5,
+          comments: 2,
+          views: 200,
+          bookmarks: 10,
+          photo: 'assets/tweet-photo2.png',
+          gif: this.sanitizer.bypassSecurityTrustResourceUrl('https://giphy.com/embed/gVlgj80ZLp9yo'),
+          timestamp: 'Dec 9'
+        }
+      ]
+    };
+  }
+
+  setActiveTab(tab: string) {
+    this.activeTab = tab;
+  }
 }
--- src/app/interface/UserProfile.ts
+++ src/app/interface/UserProfile.ts
@@ -1,19 +1,28 @@
+import { SafeResourceUrl } from "@angular/platform-browser";
+
 export interface Tweet {
     id: number;
     text: string;
     likes: number;
     retweets: number;
     timestamp: string;
+    comments: number;
+    views: number;
+    bookmarks: number;
+    photo: string;
+    gif: SafeResourceUrl;
   }
   
 export  interface UserProfile {
     name: string;
     handle: string;
     bio: string;
+    website: string;
     followers: number;
     following: number;
     joined: string;
     profileImage: string;
     coverImage: string;
     tweets: Tweet[];
+    location: string;
   }
